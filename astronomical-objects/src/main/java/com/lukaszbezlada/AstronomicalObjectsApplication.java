293b9e63d14f6e433c68c8ac33aa39b5afe03773
Łukasz Bezłada <bezi@o2.pl>
2019-07-15 19:57:32 +0200

    Spring Boot application layout

---
+++ astronomical-objects/src/main/java/com/lukaszbezlada/AstronomicalObjectsApplication.java
@@ -0,0 +1,13 @@
+package com.lukaszbezlada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstronomicalObjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstronomicalObjectsApplication.class, args);
+    }
+
+}
