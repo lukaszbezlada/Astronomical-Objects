17e710201465e330a9c1cec2ad3df9fff1306d07
Łukasz Bezłada <bezi@o2.pl>
2019-10-03 18:40:37 +0200

    Add some endpoints to controller

--- src/main/java/com/lukaszbezlada/controller/SkyObjectController.java
+++ src/main/java/com/lukaszbezlada/controller/SkyObjectController.java
@@ -1,16 +1,38 @@
 package com.lukaszbezlada.controller;
 
+import com.lukaszbezlada.entity.SkyObject;
 import com.lukaszbezlada.repository.SkyObjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
 public class SkyObjectController {
 
     @Autowired
-    private SkyObjectRepository skyObject;
+    private SkyObjectRepository skyObjectRepository;
 
+    @GetMapping("/skyObjects")
+    public Iterable<SkyObject> getSkyObjects() {
+        Iterable<SkyObject> all = skyObjectRepository.findAll();
+        return all;
+    }
 
+    @GetMapping("/getSkyObject/{name}")
+    public Optional<SkyObject> getSkyObjectByName(@PathVariable(value = "name") String name) {
+        return skyObjectRepository.findByNameContains(name);
+    }
+
+    @PostMapping(value = "/skyObject", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void addNewSkyObject(@RequestBody SkyObject skyObject) {
+        skyObjectRepository.save(skyObject);
+    }
+
+    @DeleteMapping("/skyObject/{id}")
+    public void deleteAccount(@PathVariable(value = "id") Long id) {
+        skyObjectRepository.deleteById(id);
+    }
 }
--- src/main/java/com/lukaszbezlada/repository/SkyObjectRepository.java
+++ src/main/java/com/lukaszbezlada/repository/SkyObjectRepository.java
@@ -4,10 +4,13 @@ import com.lukaszbezlada.entity.SkyObject;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SkyObjectRepository extends CrudRepository<SkyObject, Long> {
 
     List<SkyObject> findAll();
-    SkyObject findByAuthorName();
-    SkyObject findById();
+    SkyObject findByAuthorName(String authorName);
+    SkyObject findById(int id);
+    Optional<SkyObject> findByName(String name);
+    Optional<SkyObject> findByNameContains(String name);
 }
