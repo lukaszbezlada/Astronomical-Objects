524677d92ee2f89440279b8ccf4d32193d5eb453
Łukasz Bezłada <bezi@o2.pl>
2019-10-10 20:15:10 +0200

    Deleted setters and getters (use lombok)

--- src/main/java/com/lukaszbezlada/entity/User.java
+++ src/main/java/com/lukaszbezlada/entity/User.java
@@ -5,7 +5,6 @@ import lombok.*;
 import javax.persistence.*;
 import java.io.Serializable;
 
-
 @Getter
 @Setter
 @Builder
@@ -40,61 +39,5 @@ public class User implements Serializable {
 
     public User() {
         // for JPA
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
     }
 }
