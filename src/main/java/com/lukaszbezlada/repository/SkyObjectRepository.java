af5f6f9bca461c04fb807a0e1ae3fb48e3992d03
Łukasz Bezłada <bezi@o2.pl>
2020-02-29 19:11:46 +0100

    Repair removing methods

--- src/main/java/com/lukaszbezlada/repository/SkyObjectRepository.java
+++ src/main/java/com/lukaszbezlada/repository/SkyObjectRepository.java
@@ -4,7 +4,12 @@ import com.lukaszbezlada.entity.SkyObject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SkyObjectRepository extends JpaRepository<SkyObject, Long> {
+
+ //   Optional<SkyObject> findSkyObjectByNameContains(String contain);
 
 }
--- src/main/java/com/lukaszbezlada/repository/UserRoleRepository.java
+++ src/main/java/com/lukaszbezlada/repository/UserRoleRepository.java
@@ -4,4 +4,5 @@ import com.lukaszbezlada.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+    UserRole findByRole(String role);
 }
