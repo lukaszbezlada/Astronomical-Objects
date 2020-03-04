bcdee540ccff6a24ca41a2d62f8e55f24e151f7a
Łukasz Bezłada <bezi@o2.pl>
2020-03-04 22:43:35 +0100

    Adding skyObject with image

--- src/main/java/com/lukaszbezlada/controller/SkyObjectController.java
+++ src/main/java/com/lukaszbezlada/controller/SkyObjectController.java
@@ -2,9 +2,16 @@ package com.lukaszbezlada.controller;
 
 import com.lukaszbezlada.entity.SkyObject;
 import com.lukaszbezlada.utils.SkyObjectService;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+
 
 @Controller
 public class SkyObjectController {
@@ -17,8 +24,27 @@ public class SkyObjectController {
     }
 
     @PostMapping("/addSkyObject")
-    public String addSkyObject(SkyObject skyObject) {
+    @ResponseBody
+    public String addSkyObject(@RequestPart(name = "fileupload") MultipartFile file, SkyObject skyObject) { // 2
+        File uploadDirectory = new File("src/main/resources/static/img/");
+        uploadDirectory.mkdirs();    // upewniam się, że katalog do którego chcę zapisać plik istnieje, a jeśli nie, to go tworzę
+
+        try {
+            File oFile = new File("src/main/resources/static/img/" + file.getOriginalFilename());
+            OutputStream os = new FileOutputStream(oFile);
+            InputStream inputStream = file.getInputStream();
+
+            IOUtils.copy(inputStream, os); // 4
+
+            os.close();
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Wystąpił błąd podczas przesyłania pliku: " + e.getMessage();
+        }
+        skyObject.setImage("src/main/resources/static/img/" + file.getOriginalFilename());
         skyObjectService.addSkyObject(skyObject);
-        return "account";
+        return "ok!";
+
     }
 }
