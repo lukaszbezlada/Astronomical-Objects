b4d44ce2e1f74e1a6d8c47ee47d0cd5368a95e57
Łukasz Bezłada <bezi@o2.pl>
2020-04-02 15:49:46 +0200

    Try to add api to User entity with Spring Data Rest

--- src/main/java/com/lukaszbezlada/repository/UserRepository.java
+++ src/main/java/com/lukaszbezlada/repository/UserRepository.java
@@ -2,11 +2,11 @@ package com.lukaszbezlada.repository;
 
 import com.lukaszbezlada.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
-@Repository
+@RepositoryRestResource
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByLogin(String login);
